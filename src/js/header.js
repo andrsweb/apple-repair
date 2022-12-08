5fc6362d940f851295e7d3eea4e1fa78069e3d02
proflinervk <proflinervk@gmail.com>
2022-12-08 13:57:03 +0300

    added Java Script for header

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,6 @@
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
+import { setTargetElement, getTargetElement } from './common/global'
+
 document.addEventListener( 'DOMContentLoaded', () => {
 	'use strict'
 
@@ -9,16 +12,29 @@ const toogleBurgerMenu = () => {
 	const headerWrapper = document.querySelector( '.header-inner' )
 
 	burgerButton.addEventListener( 'click', () => {
+		setTargetElement( document.querySelector( '#body-lock' ) )
 
 		if( ! burgerButton && ! headerWrapper ) return
 
 		if( ! headerWrapper.classList.contains( 'opened' ) ) {
 			headerWrapper.classList.add( 'opened' )
 			burgerButton.classList.add( 'opened' )
+			disableBodyScroll( getTargetElement() )
 		} else {
 			headerWrapper.classList.remove( 'opened' )
 			burgerButton.classList.remove( 'opened' )
+			enableBodyScroll( getTargetElement() )
 		}
 	} )
 
+	window.addEventListener( 'resize', () => {
+        const windowWidth = window.innerWidth
+        const WINDOW_WIDTH_MD = 767
+
+        if( windowWidth >= WINDOW_WIDTH_MD &&  headerWrapper.classList.contains( 'opened' ) ) {
+            headerWrapper.classList.remove( 'opened' )
+            burgerButton.classList.remove( 'opened' )
+            enableBodyScroll( getTargetElement() )
+        }
+    } )
 }
