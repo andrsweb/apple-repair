0ee2506a4ae204fdaac0efa59bce8944e9de3f6d
proflinervk <proflinervk@gmail.com>
2022-12-09 23:13:21 +0300

    write js for slider

--- src/js/common/global.js
+++ src/js/common/global.js
@@ -2,3 +2,7 @@ let targetElement
 
 export const getTargetElement = () => targetElement
 export const setTargetElement = element => targetElement = element
+
+export const WINDOW_WIDTH_MD = 767
+
+export const getWindowWidth = () => window.innerWidth
