6f6d728e6e94598fa39895971204b5d1b90fbc61
proflinervk <proflinervk@gmail.com>
2022-12-20 22:31:12 +0300

    add changes in changeTime.js

--- src/js/changeTime.js
+++ src/js/changeTime.js
@@ -8,11 +8,16 @@ const changeTime = () => {
 	const startHour = document.querySelector( '.start' )
 	const endHour   = document.querySelector( '.end' )
 	const date      = new Date
-	const hours     = date.getHours()
+	let hours     = date.getHours()
 	const start     = hours - 1
-	const end       = hours + 2
+	let end       = hours + 2
 
 	if( ! startHour && ! endHour ) return
+
+	if( hours = 0 ) start = 23
+	if( hours = 24 ) end = 0
+	if( hours > 24 ) end = -24
+
 
 	startHour.innerHTML = start
 
